d75271ccccf424eb0045d04d5438b9513d4e472d
Eone-tgx <eone1024@gmail.com>
2025-05-30 20:31:03 +0300

    node-hw-01

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,16 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+    const oneContact = createFakeContact();
+    const updatedContacts = [...existingContacts, oneContact];
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.log('Помилка при додаванні контакту:', error.message);
+  }
+};
 
 addOneContact();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+    if (existingContacts.length > 0) {
+      existingContacts.pop();
+      await writeContacts(existingContacts);
+    } else {
+      console.log('Список контактів порожній');
+    }
+  } catch (error) {
+    console.log('Не вдальось видалити останній контакт:', error.message);
+  }
+};
 
 removeLastContact();
